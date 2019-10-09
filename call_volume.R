b46cc14725d0bd3ba2507df3192e88c8f40538ea
Chris Given <cmgiven@gmail.com>
2019-10-09 13:39:56 -0500

    Looking for pilot effects on call volume

---
+++ call_volume.R
@@ -0,0 +1,84 @@
+source("data.r")
+library(broom)
+
+windows <- redets %>%
+  count(recert_date) %>%
+  mutate(
+    relative_start = min(calls$date) - recert_date,
+    relative_end = max(calls$date) - recert_date
+  )
+
+weights <- data.frame(relative_time = seq(min(windows$relative_start), max(windows$relative_end)), k = 1) %>%
+  full_join(mutate(windows, k = 1), by = "k") %>%
+  mutate(valid = relative_start <= relative_time & relative_end >= relative_time) %>%
+  filter(valid) %>%
+  group_by(relative_time) %>%
+  summarize(n = sum(n)) %>%
+  transmute(relative_time, weight = 1 / n)
+
+joined_calls_redets <- calls %>%
+  left_join(redets, by = c("caller" = "phone")) %>%
+  mutate(relative_time = as.integer(date - recert_date)) %>%
+  left_join(weights, by = "relative_time")
+
+sum(!is.na(joined_calls_redets$family_id)) / nrow(joined_calls_redets)
+
+length(unique(joined_calls_redets$family_id)) / length(unique(redets$family_id))
+
+calls_by_relative_time <- joined_calls_redets %>%
+  filter(!is.na(relative_time)) %>%
+  group_by(relative_time) %>%
+  summarize(density = sum(weight))
+
+ggplot(calls_by_relative_time, aes(x = relative_time, y = density)) +
+  geom_point() +
+  geom_smooth(method = "loess", formula = y ~ x, span = .75, se = FALSE)
+
+calls_after_recert_date <- calls_by_relative_time %>%
+  filter(relative_time >= 0)
+
+decay_fit <- nls(density ~ SSasymp(relative_time, yf, y0, log_alpha), data = calls_after_recert_date)
+
+decay_fit
+
+ggplot(calls_after_recert_date, aes(x = relative_time, y = density)) +
+  geom_point() +
+  geom_line(data = augment(decay_fit), aes(y = .fitted), color = "blue", size = 1)
+
+yf <- coef(decay_fit)["yf"]
+y0 <- coef(decay_fit)["y0"]
+alpha = -exp(coef(decay_fit)["log_alpha"])
+oct_redet_count <- sum(redets$recert_date == as_date("2019-10-31"))
+
+expected_matching_call_volume <- tibble(
+  relative_time = 0:60,
+  call_volume = (yf + (y0 - yf) * exp(alpha * relative_time)) * oct_redet_count,
+  cumulative_call_volume = cumsum(call_volume)
+)
+
+ggplot(expected_matching_call_volume, aes(x = relative_time, y = cumulative_call_volume)) +
+  geom_line(color = "blue", size = 1)
+
+redets_by_pilot_participation <- redets %>%
+  filter(recert_date == as_date("2019-10-31")) %>%
+  left_join(calls, by = c("phone" = "caller")) %>%
+  group_by(family_id) %>%
+  summarize(call_count = sum(!is.na(date))) %>%
+  left_join(mutate(contacted, contacted = TRUE), by = "family_id") %>%
+  left_join(mutate(pilot_participants, opted_in = TRUE), by = "family_id") %>%
+  replace_na(list(contacted = FALSE, opted_in = FALSE)) %>%
+  mutate(group = contacted + opted_in)
+
+redets_by_pilot_participation %>%
+  group_by(group) %>%
+  summarize(n = n(), mean(call_count))
+
+redets_by_pilot_participation %>%
+  group_by(contacted) %>%
+  summarize(n = n(), mean(call_count))
+
+m <- lm(call_count ~ contacted, data = redets_by_pilot_participation)
+
+summary(m)
+
+  
---
+++ data.R
@@ -0,0 +1,84 @@
+require(tidyverse)
+require(lubridate)
+require(readxl)
+
+raw_calls = data.frame()
+for (.file in list.files("original-data/cisco")) {
+  raw_calls <- rbind(raw_calls, read_csv(
+    file.path(".", "original-data/cisco", .file),
+    col_types = cols(
+      `Node ID - Session ID - Sequence Number` = col_character(),
+      `Start Time` = col_character(),
+      `End Time` = col_character(),
+      `Contact\\Type` = col_factor(),
+      `Contact\\Disposition` = col_factor(),
+      `Originator\\Type` = col_factor(),
+      `Originator\\ID` = col_character(),
+      `Originator\\Directory Number` = col_character(),
+      `Destination\\Type` = col_integer(),
+      `Destination\\ID` = col_character(),
+      `Destination\\Directory Number` = col_character(),
+      `Called Number` = col_character(),
+      `Original Called Number` = col_character(),
+      `Application Name` = col_character(),
+      `Queue Time` = col_integer(),
+      `Talk Time` = col_integer(),
+      `Hold Time` = col_integer(),
+      `Work Time` = col_integer()
+    )
+  ))
+}
+
+calls <- raw_calls %>%
+  transmute(
+    caller = `Originator\\Directory Number`,
+    date = as_date(parse_date_time(`Start Time`, c("abd HMS Y")))
+  )
+
+raw_redets <- read_xlsx(
+    "original-data/Sept2019_Redets.xlsx",
+    col_types = "text"
+  ) %>%
+  mutate(
+    RECERT_DATE = as_date("2019-10-31")
+  )
+
+for (.sheet in 1:3) {
+  raw_redets <- rbind(raw_redets, read_xlsx(
+    "original-data/Skylight - ImpaCT Phone Numbers for Cases Up for Redet Jun-Aug2019.xlsx",
+    sheet = .sheet,
+    col_types = c("text", "text", "text", "text", "text", "text", "text", "date")
+  ))
+}
+
+redets <- raw_redets %>%
+  transmute(
+    family_id = FAMILY_ID,
+    phone = PHONE_NUMBER,
+    recert_date = RECERT_DATE
+  )
+
+raw_pilot_participants <- read_csv(
+  "original-data/Care 4 Kids Pilot Participants Tracker - Confirmed Participants.csv",
+  col_types = cols(
+    `#` = col_integer(),
+    `Case/Family ID` = col_character(),
+    `Phone Number` = col_character(),
+    `Date Contacted` = col_character(),
+    `Contacted By` = col_character(),
+    `Confirmed?` = col_logical(),
+    Notes = col_character(),
+    query = col_character(),
+    `added to database` = col_character()
+  )
+)
+
+pilot_participants <- raw_pilot_participants %>%
+  transmute(
+    family_id = `Case/Family ID`
+  ) %>%
+  filter(!is.na(family_id)) %>%
+  unique()
+
+contacted <- read_csv("original-data/contacted.csv", col_types = list(col_character())) %>%
+  unique()
